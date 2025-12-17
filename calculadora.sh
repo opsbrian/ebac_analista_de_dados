f52bea994571e26e45e5c816f3ad3225fe504fc1
opsbrian <168209568+opsbrian@users.noreply.github.com>
2025-12-17 17:07:46 +0100

    Update calculadora.sh
    Atualização de script.

---
+++ calculadora.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Exibe a mensagem no terminal
+echo "Iniciando o processo..."
+
+# Comando para rodar o Python
+python3 calculadora_v1.py
+
+# Exibe a mensagem no terminal ao finalizar o processo.
+echo "Processo finalizado."
